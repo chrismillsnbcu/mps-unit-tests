8ab978f0ff1beb91874bbbc0feef31c7c6b85729
Ruben R <ruben.rajkowski@gmail.com>
2014-10-27 10:37:57 -0700

    update comments

--- spec/mps_spec_v2.js
+++ spec/mps_spec_v2.js
@@ -1,8 +1,8 @@
-/***** Javascript Tests for mps._append(). *****/
-/***** Frameworks: MochaJS, PhantomJS and Xunit Reporting for Sonar Coverage *****/
+/***** Javascript Unit Tests for mps._append(). *****/
+/***** Frameworks: MochaJS, PhantomJS and Xunit Reports for SonarQube Coverage *****/
 /***** Date: 10/24/14  *****/
 
-// Allow chai to run asynchronous Mocha tests
+// Allow chai to run asynchronous Mocha tests: see try-catch below
 window.expect = chai.expect;
 describe("MPS core _append tests", function (done) {
   // Global vars.
@@ -22,8 +22,42 @@ describe("MPS core _append tests", function (done) {
         done( e ) // failure:
       }
     }, 100 );
-
-
   });
 
+  it("Create and append a script", function() {
+    // Create script, check for immediate execution.
+    var script = '<script>a = true;</script>';
+    expect(a).to.be.false;
+    // Append script and check it is executed.
+    mps._append(mps._select('head'), script);
+    setTimeout( function () {
+      // Called from the event loop, not it()
+      try {
+        expect(a).to.be.true;
+        done()    // success:
+      } catch( e ) {
+        done( e ) // failure:
+      }
+    }, 100 );
+  });
+
+
+  it("Create and append a multiline string", function() {
+    var str = '<div>\r\nsome text\n<p>some p</p>\nmore text\r\n</div>';
+    mps._append(mps._select('#append-html-multi'),str);
+    var elem = document.getElementById('append-html-multi');
+    // Check html is appended correctly by verifying node name and child types.
+    setTimeout( function () {
+      try {
+        expect(elem.nodeName.toUpperCase()).to.be('DIV');
+        expect(elem.firstChild.childNodes[0].nodeType).to.be(3);
+        expect(elem.firstChild.childNodes[1].nodeType).to.be(1);
+        expect(elem.firstChild.lastChild.nodeType).to.be(3);
+        done()    // success:
+      } catch( e ) {
+        done( e ) // failure:
+      }
+    }, 100 );
+
+  });
 });  // end describe
